3f29667b0a54cb172c0f6afb9790eb96cb4476d9
default <dalvath3700@gmail.com>
2022-10-11 22:12:40 +0530

    added next-api/ folder

---
+++ libs/nextjs/next-api/data/comments.js
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is the first commit",
+  },
+  {
+    id: 2,
+    text: "This is the second commit",
+  },
+  {
+    id: 3,
+    text: "This is the third commit",
+  },
+];
