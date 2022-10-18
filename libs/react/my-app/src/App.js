7d9ef8e6d9d72d485fe265ba29a26c81e4de6fb5
default <dalvath3700@gmail.com>
2022-10-18 12:49:37 +0530

    my-app react: button with useState

--- libs/react/my-app/src/App.js
+++ libs/react/my-app/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 
 function MyButton() {
+  const [count, setCount] = useState(0); // start count from '0'
+
   function handleClick() {
-    alert('button clicked');
+    // alert('button clicked');
+    setCount(count + 1);
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
 
 function App() {
-  return <MyButton />;
+  return (
+    <div>
+      <h2>Counters that update separately</h2>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
 }
 
 export default App;
